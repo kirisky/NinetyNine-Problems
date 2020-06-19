31cd7783d7d546f83e679e07845e0463117765e5
kirisky <youandox@gmail.com>
2020-06-19 21:33:13 +0300

    Add #10 problem

---
+++ OCaml/Lists/P10.ml
@@ -0,0 +1,11 @@
+(* 99 Problems *)
+(* 10 *)
+
+
+(* Run-length encoding of a list. (easy)
+ * 
+ * # encode ["a";"a";"a";"a";"b";"c";"c";"a";"a";"d";"e";"e";"e";"e"];;
+ * - : (int * string) list =
+ * [(4, "a"); (1, "b"); (2, "c"); (2, "a"); (1, "d"); (4, "e")]
+ *
+ *)
