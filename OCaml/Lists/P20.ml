fb6c4b327e7d85ccd777d532fd82e11a7d97a4f1
kirisky <youandox@gmail.com>
2020-06-21 21:34:10 +0300

    Add 20

---
+++ OCaml/Lists/P20.ml
@@ -0,0 +1,12 @@
+(* 99 Problems *)
+(* 19 *)
+
+
+(* Remove the K'th element from a list. (easy) 
+ * 
+ * The first element of the list is numbered 0, the second 1,...
+ *
+ * # remove_at 1 ["a";"b";"c";"d"];;
+ * - : string list = ["a"; "c"; "d"]
+ * 
+ *)
